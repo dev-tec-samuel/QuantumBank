ffac78c542cb07c40b5ba128287914acb2842d0b
Samuel da Rosa <sr006546@cfjl.com.br>
2024-09-04 23:11:54 +0000

    teste

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   constructor(private router:Router){}
-
+  // teste
   logar() {
     this.router.navigate(['/home'])
   }
