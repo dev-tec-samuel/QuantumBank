f93931f7f62bc29684471c62674aaa9ec1c2bb38
Samuel da Rosa <sr006546@cfjl.com.br>
2024-09-05 00:49:50 +0000

    Auste pra testes

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent{
+  constructor(private router:Router){}
+
+  verExtrato() {
+    this.router.navigate(['/extrato'])
+  }
 
 }
