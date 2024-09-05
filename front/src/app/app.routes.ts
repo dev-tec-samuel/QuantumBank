ce5020f09306c965c18fc6b233b7f88b644c0b97
Samuel da Rosa <sr006546@cfjl.com.br>
2024-09-05 00:50:04 +0000

    Demais alterações

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'extrato', component: ExtratoComponent }
 ];
--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -11,7 +11,6 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   constructor(private router:Router){}
-  // teste
   logar() {
     this.router.navigate(['/home'])
   }
