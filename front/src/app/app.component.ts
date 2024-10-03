4f1ac75c4b534a1cfdfe5f478bfff05a72868971
Samuel da Rosa <sr006546@cfjl.com.br>
2024-10-03 02:20:22 +0000

    Add novos imports e configs

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HomeComponent],
+  imports: [RouterOutlet, CommonModule, HomeComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -6,5 +7,6 @@ import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'extrato', component: ExtratoComponent }
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'cadastro', component: CadastroComponent}
 ];
