3d8bbb013347933f69a66a565ce30891db4e5c96
dev-tec-samuel <sr0127775@setrem.com.br>
2024-11-27 23:29:28 -0300

    Ajuste funções backend

--- api/src/main/java/br/com/poo/poobank/controller/CorrentistaController.java
+++ api/src/main/java/br/com/poo/poobank/controller/CorrentistaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import br.com.poo.poobank.domain.Correntista;
 import br.com.poo.poobank.repository.CorrentistaRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/correntista")
 public class CorrentistaController {
 
@@ -40,7 +42,7 @@ public class CorrentistaController {
     return repository.searchCorrentistaByCpf(cpf);
   }
 
-  @PostMapping
+  @PostMapping("/insertCorrentista")
   public ResponseEntity<Correntista> insertCorrentista(@RequestBody Correntista correntista)
   {
     Correntista newCorrentista = repository.save(correntista);
