5d80ad39db52e08c8e3d860a361b0b089f01669a
dev-tec-samuel <sr0127775@setrem.com.br>
2024-11-28 20:28:55 -0300

    Novas rotas da ações do PIX

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { EnviarPixComponent } from './enviar-pix/enviar-pix.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { OpcaoPixComponent } from './opcao-pix/opcao-pix.component';
 import { ProvaPooComponent } from './prova-poo/prova-poo.component';
+import { ReceberPixComponent } from './receber-pix/receber-pix.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -11,4 +14,7 @@ export const routes: Routes = [
   { path: 'extrato', component: ExtratoComponent },
   { path: 'cadastro', component: CadastroComponent},
   { path: 'prova-poo', component: ProvaPooComponent},
+  { path: 'enviar-pix', component: EnviarPixComponent},
+  { path: 'opcao-pix', component: OpcaoPixComponent},
+  { path: 'receber-pix', component: ReceberPixComponent},
 ];
