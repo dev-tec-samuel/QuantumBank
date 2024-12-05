37c59ac981e3749b5ba0226fca42c2d26b951352
dev-tec-samuel <sr0127775@setrem.com.br>
2024-12-05 00:19:54 -0300

    Criação de variavel global e ajuste de chamada de método

--- front/src/app/correntista.service.ts
+++ front/src/app/correntista.service.ts
@@ -12,6 +12,8 @@ export class CorrentistaService {
     private http: HttpClient
   ) { }
 
+  correntistaLogado:number = 0;
+
   // Método para cadastrar um novo correntista
   public cadastrar(correntista: Correntista): Observable<any> {
     return this.http.post("http://localhost:8081/correntista/insertCorrentista", correntista);
@@ -20,5 +22,10 @@ export class CorrentistaService {
   // Método para obter a lista de correntistas
   public listarCorrentistas(): Observable<Correntista[]> {
     return this.http.get<Correntista[]>("http://localhost:8081/correntista");
+  }
+
+   // Método para obter a lista de correntistas
+   public buscarCorrentistaPeloId(id:number): Observable<Correntista[]> {
+    return this.http.get<Correntista[]>("http://localhost:8081/correntista/id/" + id);
   }
 }
